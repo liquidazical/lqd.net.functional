047bb06a15f834921aca01042acf5148bf6d1d14
Paul Moore <wipmoore@outlook.com>
2016-11-22 17:14:55 +0000

    Creat Version 0.1.0

--- lqd.net.functional/Properties/AssemblyInfo.cs
+++ lqd.net.functional/Properties/AssemblyInfo.cs
@@ -1,36 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("lqd.net.functional")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyDescription("A set of functions and classes that aid a more functional approach in C#")]
 [assembly: AssemblyProduct("lqd.net.functional")]
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("45397537-2aa3-42ad-9ffa-e23fce673ce0")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
--- readme.md
+++ readme.md
@@ -7,6 +7,8 @@ Provides a set of functions and object that are intended to aid applying a funct
 
 ### Next Version 
 
+### Vr 0.1.0
+
 * fmap - extension method that applies a function an a obect ( it is like map for objects )
 * Maybe - an implementation of the Maybe monad
 * Result - an implementation of a monad that represents the states of either a success with a value or an error with a description in the form of a ResultError
