3adaac78932182a3f9ae3a7141e9a37aa0c2d07f
Paul Moore <wipmoore@outlook.com>
2016-11-22 12:08:09 +0000

    Implement Maybe

---
+++ lqd.net.functional.test/Maybe_will.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Xunit;
+
+namespace lqd.net.functional.test {
+
+    public class Maybe_will {
+
+        // done: allow you to create a maybe with a value
+        // done: throw a argument null exception if you supply a null when trying to create a maybe with a value
+        // done: allow you to create a maybe without a value
+        // done: allow you to match against the maybe and specify a function to perform if it has a value
+        // done: allow you to match against the maybe and specify a function to perform if it has no value
+        // done: allow you to check if there is a value
+        // done: allow you to specify an action to take if there is a value
+        // done: allow you to specify an action to take if there is no value
+        // done: allow you to specify an action if there is a value
+        // done: allow you to specify an action if there is not a value
+
+
+        
+        [Fact]
+        public void allow_you_to_create_a_maybe_with_a_value() {
+
+            var a = new object();
+            var m = Maybe<object>.just( a );
+
+
+            Assert.True( m is Maybe<object> );
+        }
+        
+        
+        [Fact]
+        public void throw_a_argument_null_exception_if_you_supply_a_null_when_trying_to_create_a_maybe_with_a_value() {
+
+            var act = (Action)( () => Maybe<object>.just( null ) );
+
+
+            Assert.Throws<ArgumentNullException>( act );
+        }
+
+
+        [Fact]
+        public void allow_you_to_create_a_maybe_without_a_value() {
+
+            var m = Maybe<object>.nothing();
+
+            Assert.True( m is Maybe<object> );
+        }
+
+
+        [Fact]
+        public void allow_you_to_match_against_the_maybe_and_specify_a_function_to_perform_if_it_has_a_value () {
+
+            var a = new object();
+            var m = Maybe<object>.just( a );
+
+            var b = m.Match(
+                just: o => o
+               ,nothing: () => new object()
+            );
+       
+
+            Assert.Equal( a, b );
+        }
+
+        [Fact]
+        public void allow_you_to_match_against_the_maybe_and_specify_a_function_to_perform_if_it_has_no_value() {
+
+            var a = new object();
+            var m = Maybe<object>.nothing();
+
+            var b = m.Match(
+                 just: o => { throw new Exception( "This case should never execute" ); }
+                ,nothing: () => a
+            );
+
+            Assert.Equal( a, b );
+        }
+
+        [Fact]
+        public void allow_you_to_check_if_there_is_a_value() {
+
+            var m1 = Maybe<object>.just( new object() );
+            var m2 = Maybe<object>.nothing();
+
+
+            Assert.True( m1.HasValue() );
+            Assert.False( m2.HasValue() );
+        }
+
+        [Fact]
+        public void allow_you_to_specify_an_action_to_take_if_there_is_a_value() {
+
+            var a = new object();
+            var b = new Object();
+
+            var m = Maybe<object>.just( a );
+
+            m.Match(
+               just: n => { b = n;  }
+              ,nothing : () => { throw new ArgumentNullException( "This case should never be called" ); }
+            );
+
+
+            Assert.Equal( a, b );
+        }
+
+        [Fact]
+        public void allow_you_to_specify_an_action_to_take_if_there_is_no_value() {
+
+            var a = new object();
+            var b = new Object();
+
+            var m = Maybe<object>.nothing( );
+
+            m.Match(
+               just: n => { throw new ArgumentNullException( "This case should never be called" ); }
+              ,nothing : () => { b = a; }
+            );
+
+
+            Assert.Equal( a, b );
+        }
+
+
+        [Fact]
+        public void allow_you_to_specify_an_action_if_there_is_a_value() {
+
+            var ma = Maybe<object>.just( new object() );
+            var mb = Maybe<object>.just( new object() );
+
+
+            Assert.Equal( mb, ma.Bind( a => mb ) );
+
+        }
+
+        [Fact]
+        public void allow_you_to_specify_an_action_if_there_is_not_a_value() {
+
+            var ma = Maybe<object>.nothing();
+            var mb = Maybe<object>.just( new object() );
+
+
+            Assert.Equal( mb, ma.IfNothing( () => mb ) );
+        }
+
+    }
+}
---
+++ lqd.net.functional/Maybe.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace lqd.net.functional {
+
+    /// <summary>
+    /// Implementation of the Maybe monad
+    /// </summary>
+    public class Maybe<P> {
+
+
+        /// <summary>
+        /// Creates a Maybe that represents having a value
+        /// </summary>
+        public static Maybe<P> just
+                               ( P value  ) {
+
+            return new Just( value );
+        }
+
+        /// <summary>
+        /// Creates a Maybe that represents having no value
+        /// </summary>
+        public static Maybe<P> nothing
+                                 ( ) {
+
+            return new Nothing();
+        }
+
+        /// <summary>
+        /// Returns true if the Maybe represent a value
+        /// </summary>
+        public bool HasValue() {
+
+          return
+            Match(
+               just: v => true
+              ,nothing: () => false
+            );
+        }        
+
+        public Maybe<Q> Bind<Q>
+                         ( Func<P, Maybe<Q>> f ) {
+
+          if ( f == null ) throw new ArgumentNullException( nameof( f ) );
+
+
+          return 
+            Match(
+               just: f 
+              ,nothing: () => Maybe<Q>.nothing()
+            );
+
+        }
+
+        public Maybe<P> IfNothing
+                         ( Func<Maybe<P>> f )  {
+
+          if ( f == null ) throw new ArgumentNullException( nameof( f ) );
+
+          return
+            Match(  
+               just: v => this
+              ,nothing: f
+            );
+        }
+
+
+        /// <summary>
+        /// Allow you to specify what should be returned for either case of 
+        /// having a value or not having a value
+        /// </summary>
+        public Q Match<Q>
+                  ( Func<P,Q> just  
+                  , Func<Q> nothing ) {
+
+           if ( just == null ) throw new ArgumentNullException( nameof( just ) );
+           if ( nothing == null ) throw new ArgumentNullException( nameof( nothing ) );
+
+
+           if ( this is Just ) {
+              return just( (this as Just).value );
+           }
+
+           if ( this is Nothing ) {
+               return nothing( );
+           }
+
+           throw new Exception( "Case not handeled" );
+        } 
+
+
+        /// <summary>
+        /// Allow you to specify what should be performed for either case of 
+        /// having a value or not having a value
+        /// </summary>
+        public void Match
+                     ( Action<P> just 
+                     , Action nothing ) {
+
+            Match(
+               just: p => { just( p ); return new object();  }
+              ,nothing: () => { nothing(); return new object();  }
+            );
+
+        }
+
+
+        private class Just
+                       : Maybe<P> {
+
+            public P value { get; private set; }
+
+            public Just
+                    ( P value ) {
+        
+                if ( value == null ) throw new ArgumentNullException( nameof( value ) );
+
+
+                this.value = value;
+            }
+        }
+        
+        private class Nothing
+                       : Maybe<P> { }
+               
+
+        private Maybe() {  } // ensure that you can only create a maybe via the factory methods just and nothing
+    }
+
+}
